7dee31ccab308a0b3ede503b6ece0d6b2a2e559a
Taha <taha100_100@yahoo.com>
2024-11-26 20:09:43 +0200

    Added initial models

---
+++ src/models/categoryModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Category Name is required"],
+    maxlength: [30, "Category Name cannot exceed 30 characters"],
+  },
+  displayOrder: {
+    type: Number,
+    required: [true, "Display Order is required"],
+    min: [1, "Display Order must be at least 1"],
+    max: [100, "Display Order cannot exceed 100"],
+  },
+  description: String,
+});
+
+const Category = mongoose.model("Category", categorySchema);
+
+module.exports = Category;
---
+++ src/models/productModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Title is required"],
+  },
+  description: {
+    type: String,
+    required: [true, "Description is required"],
+  },
+  ISBN: {
+    type: String,
+  },
+  author: {
+    type: String,
+    required: [true, "Author is required"],
+  },
+
+  price: {
+    type: Number,
+    required: [true, "Price for 1-50 is required"],
+    min: [1, "Price must be at least 1"],
+    max: [1000, "Price cannot exceed 1000"],
+  },
+
+  categoryId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "category",
+    required: true,
+  },
+  productImages: [
+    {
+      type: String,
+      default: "/public/img/products/default.jpg",
+    },
+  ],
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
