afb4e58d934bb20a813dfbfe8ec108c9886dde1a
Taha <taha100_100@yahoo.com>
2024-12-18 03:49:31 +0200

    Added initial needed packages for apis security and optimization

--- src/app.js
+++ src/app.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const hpp = require('hpp');
+const xss = require('xss-clean');
+const cookieParser = require('cookie-parser');
+const compression = require('compression');
 const AppError = require('./appError');
 const globalErrorHandler = require('./controllers/errorController');
 const categoryRouter = require('./routes/categoryRoutes');
@@ -8,14 +15,26 @@ const cartRouter = require('./routes/cartRoutes');
 const orderRouter = require('./routes/orderRoutes');
 
 const app = express();
-
+app.use(cors());
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  limit: 100,
+  standardHeaders: 'draft-7',
+  legacyHeaders: false,
+  message: 'Too many requests from this IP, please try again after an hour',
+});
+app.use(helmet({ contentSecurityPolicy: false }));
+app.use('/api', limiter);
+app.use(xss());
+app.use(hpp());
+app.use(cookieParser());
 app.use(express.json());
 app.use('/api/v1/categories', categoryRouter);
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/carts', cartRouter);
 app.use('/api/v1/orders', orderRouter);
-
+app.use(compression());
 app.all('*', (err, req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
